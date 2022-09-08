2207f69b38c4db8cdb2c955ce3c2c91713d5bc2f
mohmmed fathi <mohmmedfathi.123@gmail.com>
2022-09-08 16:32:36 +0200

    delete something"

---  did_django_schedule_jobs / updater.py
+++
@@ -1,9 +0,0 @@
-
-from datetime import datetime
-from apscheduler.schedulers.background import BackgroundScheduler
-from  jobs.py import do_anything
-
-def start():
-	scheduler = BackgroundScheduler()
-	scheduler.add_job(do_anything, 'interval', seconds=2)
-	scheduler.start()
